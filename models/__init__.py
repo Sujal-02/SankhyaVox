62927486e3bf102f223f44519b0b1317ec68e7b9
devarabhavana <devarabhavana30102003@gmail.com>
2026-04-11 22:15:31 +0530

    Revert "HMM v3.0 (untrained): Pomegranate backend for fast implementation"
    This reverts commit 1e10729b5d7f81406b3308ed25f3e88d6c27f0f6.

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 from models.gmm_classifier import GMMClassifier
-from models.hmm_classifier import SankhyaHMM          # active (pomegranate)
-# from models.hmm_classifier_old import SankhyaHMM    # legacy (hmmlearn)
+from models.hmm_classifier import SankhyaHMM
 from models.knn_dtw_classifier import KNNDTWClassifier
 from models.svm_classifier import SVMClassifier
 
